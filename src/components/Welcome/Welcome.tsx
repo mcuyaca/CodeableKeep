01ed79f854b4bc0a07989f0f7cc134d24277a81b
mcuya.ca <mcuya.ca@gmail.com>
2024-02-28 21:50:11 -0500

    add alert message

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -43,7 +43,7 @@ function Welcome(props: WelcomeProps) {
         username
       </p>
       <input
-        className="mb-[13px] flex  h-[32px] w-[241px] rounded-2xl border border-[#18181B] px-4 py-2 text-base text-[#737373] "
+        className="mb-[13px] flex h-[32px] w-[241px] rounded-2xl border border-[#18181B] px-4 py-2 text-base text-[#737373] "
         type="text"
         placeholder="some-user"
         value={username}
@@ -55,6 +55,40 @@ function Welcome(props: WelcomeProps) {
       >
         Enter
       </button>
+
+      <div className=" mt-12 flex min-w-fit justify-center gap-4 rounded-lg border border-[#18181B]  p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-info alert__icon flex"
+        >
+          <circle cx="12" cy="12" r="10"></circle>
+          <path d="M12 16v-4"></path>
+          <path d="M12 8h.01"></path>
+        </svg>
+        <div className="flex w-[270px] flex-col gap-1 text-[#18181B] ">
+          <h5 className="font-semibold text-[#18181B] ">
+            Servidor en ahorro de energía{" "}
+          </h5>
+          <p className="text-light text-balance text-sm font-light text-gray-500">
+            Espera 30 segundos para que se inicie. Puedes forzar el encendido{" "}
+            <a
+              className="text-semibold font-bold text-[#18181B] underline underline-offset-2 "
+              href={API_URL}
+            >
+              aquí
+            </a>
+            .
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
